280f7aaf2bdd4d181a1fdc06cdc20e4d6d98d6e0
Tohin666 <tohin666@gmail.com>
2019-05-03 13:42:05 +0300

    added db credentials

--- config/db.php
+++ config/db.php
@@ -1,14 +1,35 @@
 <?php
 
-return [
+if ($_SERVER['SERVER_NAME'] == "thawing-island-242342379.herokuapp.com") {
+    $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    $host = $url["host"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $dbname = substr($url["path"], 1);
+} else {
+    $host = 'localhost';
+    $dbname = 'yii2basic';
+    $username = 'root';
+    $password = '';
+}
+$dbConfig = [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => "mysql:host={$host};dbname={$dbname}",
+    'username' => $username,
+    'password' => $password,
     'charset' => 'utf8',
-
-    // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
 ];
+return $dbConfig;
+
+//return [
+//    'class' => 'yii\db\Connection',
+//    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+//    'username' => 'root',
+//    'password' => '',
+//    'charset' => 'utf8',
+//
+//    // Schema cache options (for production environment)
+//    //'enableSchemaCache' => true,
+//    //'schemaCacheDuration' => 60,
+//    //'schemaCache' => 'cache',
+//];
